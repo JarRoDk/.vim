c171a84e8535e487b1e3f7c4243f8d4321cfda41
Jaroslaw Nyka <jaroslawx.nyka@intel.com>
2016-12-16 10:42:59 +0100

    remove .bashrc from git repo
    - remove bashrc from repo
    - create script to add lines by config.sh
    - create powerline-run.sh for universal powerline commands

---
+++ powerline-run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+powerline-daemon -q
+POWERLINE_BASH_CONTINUATION=1
+POWERLINE_BASH_SELECT=1
+  if [ -f /usr/lib/python2.6/site-packages/powerline/bindings/bash/powerline.sh ]; then
+        . /usr/lib/python2.6/site-packages/powerline/bindings/bash/powerline.sh
+elif [ -f /usr/lib/python2.7/site-packages/powerline/bindings/bash/powerline.sh ]; then
+        . /usr/lib/python2.7/site-packages/powerline/bindings/bash/powerline.sh
+elif [ -f /usr/local/lib/python2.7/dist-packages/powerline/bindings/bash/powerline.sh ]; then
+        . /usr/local/lib/python2.7/dist-packages/powerline/bindings/bash/powerline.sh
+fi
+
